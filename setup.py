9c27462823f88550e32be5934091335aee997e5a
Ben Caunt <bdcaunt@gmail.com>
2019-12-12 20:14:08 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,11 +5,23 @@ setup(
     url='https://github.com/BenCaunt8300/stockit',
     author='Ben Caunt',
     author_email='bdcaunt@gmail.com',
-    author_instagram='@bencaunt1232',
     packages=['stockit'],
     install_requires=['pandas','numpy','matplotlib','tqdm','sklearn'],
     version='1.0',
     license='Apache License 2.0',
-    description='python library for easy stock analysis and prediction'
+    description='python library for easy stock analysis and future price estimations. FOR EDUCATIONAL PURPOSES ONLY, NOT INVESTMENT ADVICE'
+    classifiers=[
+      'Development Status :: 3 - Alpha',
+      'Intended Audience :: Developers',
+      'Topic :: Software Development :: Build Tools',
+      'License :: Apache 2.0',
+      'Programming Language :: Python :: 3',
+      'Programming Language :: Python :: 3.4',
+      'Programming Language :: Python :: 3.5',
+      'Programming Language :: Python :: 3.6',
+      'Programming Language :: Python :: 3.7',
+      'Programming Language :: Python :: 3.8',
+    ],
+
 )
 
