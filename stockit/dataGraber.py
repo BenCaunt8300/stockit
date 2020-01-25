28792d8e51586954bc28315e6cc4ae3dc61b158f
BenCaunt8300 <bdcaunt@gmail.com>
2020-01-25 18:07:01 -0500

    updated returnData and downloadData functions to have a 'year' setting that tells the function how much  data to dwonload

--- stockit/dataGraber.py
+++ stockit/dataGraber.py
@@ -5,12 +5,15 @@ import pandas_datareader as web
 #datetime is a Python module
 import datetime
 
-def returnData(stock_ticket):
+DAYS_IN_YEAR = 365
+def returnData(stock_ticket, years = 3):
     '''
     returns pandas dataframe containing stock prices from the past year of the 'stock_ticket'
+
+    Years is the number of years of data that is collected
     '''
     #datetime.datetime is a data type within the datetime module
-    start = datetime.datetime.today() - datetime.timedelta(days=365)
+    start = datetime.datetime.today() - datetime.timedelta(days=int(DAYS_IN_YEAR * years))
 
     end = datetime.date.today()
     # download data, if data not found, raise exception
@@ -23,12 +26,14 @@ def returnData(stock_ticket):
 
 
 
-def downloadData(stock_ticket):
+def downloadData(stock_ticket, years = 3):
     '''
     downloads stock prices from the past year of the 'stock_ticket' and saves it in a file called 'stock_ticket.csv'
+
+    Years is the number of years of data that is collected
     '''
     #datetime.datetime is a data type within the datetime module
-    start = datetime.datetime.today() - datetime.timedelta(days=365)
+    start = datetime.datetime.today() - datetime.timedelta(days=int(DAYS_IN_YEAR * years))
 
     end = datetime.date.today()
     # download data, if data not found, raise exception
